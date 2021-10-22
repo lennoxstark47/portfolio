6ba86d1623edf28be1ea86b9c91583d642307656
lennoxstark47 <twisamhaldar@gmail.com>
2021-10-22 15:06:21 +0530

    completed but changes needed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
-
+import React from 'react';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 export default function App() {
-  return "App";
+	return (
+		<main className='text-gray-400 bg-gray-900 body-font'>
+			<Navbar />
+			<About />
+			<Projects />
+			<Skills />
+			<Testimonials />
+			<Contact />
+		</main>
+	);
 }
